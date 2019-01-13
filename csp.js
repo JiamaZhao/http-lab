2f2a2dec5355ae27785c4df700a426a2bb8114bc
jiama <dearwinter@foxmail.com>
2019-01-14 07:45:15 +0800

    feat: csp

--- csp.js
+++ csp.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 http.createServer(function(req, res){
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8',
+        'Content-Security-Policy-Report-Only': 'default-src \'self\'; form-action \'self\'; report-uri /report '
     });
     const html = fs.readFileSync('./test.html', 'utf-8');
     res.end(html);
